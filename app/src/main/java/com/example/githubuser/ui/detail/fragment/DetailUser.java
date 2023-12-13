ec96c85809cfedeeb82940f4a4cf056f3c6e7032
Ade Putra <adeputra.c@gmail.com>
2023-12-13 12:07:09 +0700

    CheckPoint()

--- app/src/main/java/com/example/githubuser/ui/detail/fragment/DetailUser.java
+++ app/src/main/java/com/example/githubuser/ui/detail/fragment/DetailUser.java
@@ -91,7 +91,6 @@ public class DetailUser extends AppCompatActivity {
                 } else if (result instanceof Result.Success){
                     viewModel.selectedUserOffline(userGitSelect.getUserName()).observe(this, listSelected -> {
                         userGitAdapter.submitList(listSelected);
-                        //Toast.makeText(this,listSelected.get(0).getNamaUser(),Toast.LENGTH_LONG).show();
                         binding.tvnamaUser.setText(listSelected.get(0).getNamaUser());
                         binding.tvFollowers.setText(listSelected.get(0).getFollowers_url()+" Followers");
                         binding.tvFollowing.setText(listSelected.get(0).getFollowing_url()+" Following");
