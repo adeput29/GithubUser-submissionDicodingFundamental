470884be4301aa8606cd1df13f409135c709a48c
Ade Putra <adeputra.c@gmail.com>
2023-12-16 22:23:03 +0700

    add credit

---
+++ app/src/main/java/com/example/githubuser/ui/about.java
@@ -0,0 +1,16 @@
+package com.example.githubuser.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.githubuser.R;
+
+public class about extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
